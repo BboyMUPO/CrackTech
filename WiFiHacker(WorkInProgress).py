ac58ca93c5d87f0bb2c5f420d68dee5c22e1ebfd
BboyMUPO <sarkanyalex214@gmail.com>
2013-08-29 16:25:07 +0200

    WiFiHacker(WorkInProgress(WIP))This app will crack up passwords of locked wifis.In the future it will use encryption
    WiFiHacker(WorkInProgress(WIP))This app will crack up passwords of locked wifis.In the future it will use encryption

---
+++ WiFiHacker(WorkInProgress).py
@@ -0,0 +1,18 @@
+#I DON'T GET THE RISK FOR ANYTHING
+import socket
+import os
+import appuifw
+
+def crack():
+    your_list = 'qwertzuiopasdfghjklíyxcvbnm' #latters you want to use in brute-force attack
+    complete_list = []
+    for current in xrange(2): #how long the password is default 2
+        a = [i for i in your_list]
+        for y in xrange(current):
+            a = [x+i for i in your_list for x in a]
+        complete_list = complete_list+a #Yeah it's the brute force
+
+def connect():
+    apid = socket.select_acces_point() #On some deviece wlantools.scan() freeze
+    apo = socket.acces_point(apid)
+    apo.start
