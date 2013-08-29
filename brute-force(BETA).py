b9678fb6e1fba941171c5dbefa19b638688e0f41
BboyMUPO <sarkanyalex214@gmail.com>
2013-08-29 16:15:36 +0200

    This brute-force now only print the passwords but don't type them.It will be implented in the future

---
+++ brute-force(BETA).py
@@ -0,0 +1,10 @@
+#I DON'T GET THE RISK FOR ANYTHING
+your_list = 'qwertzuiopasdfghjklíyxcvbnm' #latters you want to use in brute-force attack
+complete_list = []
+for current in xrange(2): #how long the password is default 2
+    a = [i for i in your_list]
+    for y in xrange(current):
+        a = [x+i for i in your_list for x in a]
+    complete_list = complete_list+a
+    
+print (complete_list)
