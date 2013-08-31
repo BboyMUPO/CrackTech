b6e9057e91761aae6c1316f8800c37e8bbadb913
BboyMUPO <sarkanyalex214@gmail.com>
2013-08-31 17:03:56 +0800

    Create acHack(forAfriend).py

---
+++ acHack(forAfriend).py
@@ -0,0 +1,5 @@
+import urllib
+import appuifw
+
+username = appuifw.query(u"Write your username here:", "text"#Ez kéri majd a felhasználó neved kun XD
+password = appuifw.query (u"Write your password here:", "code" #Ez kéri a jelszavad am most abbahagyom h tudjunk chatelni XD
