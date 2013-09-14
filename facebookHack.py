9318c7567d3e2fc2dcdc805a96dd9793c8c863f7
BboyMUPO <sarkanyalex214@gmail.com>
2013-09-14 18:54:26 +0200

    Create facebookHack.py

---
+++ facebookHack.py
@@ -0,0 +1,17 @@
+import urllib
+import md5
+import appuifw
+
+def crack():
+    your_list = 'qwertzuiopasdfghjklíyxcvbnm' #latters you want to use in brute-force attack
+    complete_list = []
+    for current in xrange(2): #how long the password is default 2
+        a = [i for i in your_list]
+        for y in xrange(current):
+            a = [x+i for i in your_list for x in a]
+        complete_list = complete_list+a
+
+target = appuifw.qurey(u"Facebook id:", "text")
+urllib.urlopen(target)
+password = crack()
+#XD feltörök minden faszt facen peti XD
