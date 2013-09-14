1649c3d74942903e67197e8362540e95d6d2c667
BboyMUPO <sarkanyalex214@gmail.com>
2013-09-14 11:39:07 +0200

    Update brute-force(BETA).py

--- brute-force(BETA).py
+++ brute-force(BETA).py
@@ -1,4 +1,5 @@
 #I DON'T GET THE RISK FOR ANYTHING
+
 your_list = 'qwertzuiopasdfghjklíyxcvbnm' #latters you want to use in brute-force attack
 complete_list = []
 for current in xrange(2): #how long the password is default 2
@@ -8,3 +9,9 @@ for current in xrange(2): #how long the password is default 2
     complete_list = complete_list+a
     
 print (complete_list)
+
+testpass = ad
+passs = complete_list
+
+if passs == testpass:
+    print(passs)
