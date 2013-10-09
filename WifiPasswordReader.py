3c674ee6a95170e170f816fe04dadcfe853637fa
BboyMUPO <sarkanyalex214@gmail.com>
2013-10-09 20:17:55 +0200

    Create WifiPasswordReader.py

---
+++ WifiPasswordReader.py
@@ -0,0 +1,24 @@
+#I don't get the risk for anything
+import os
+import appuifw
+
+SAVEDIR = appuifw.query(u"Path for the files:", "text")
+if not os.path.isdir(SAVEDIR):
+    os.makedirs(SAVEDIR)
+
+targetf = c:\private\10202be9\persists\cccccc00.cre
+f=open(targetf,'rt')
+try:
+    f=open(targetf,'rt')
+    try:
+        content = f.read()
+        config=eval(content)
+        f.close()
+    except:
+        print 'Can not read file'
+except:
+    print 'Can not open file'
+
+file = open(targetf, 'wb')
+file.write(data, SAVEDIR)
+file.close
