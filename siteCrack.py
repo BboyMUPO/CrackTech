ea13bf9e8303e632d9daa9a4521c220ff8106504
BboyMUPO <sarkanyalex214@gmail.com>
2013-10-09 19:59:50 +0200

    Update and rename siteCrack(WIP).py to siteCrack.py

---
+++ siteCrack.py
@@ -0,0 +1,29 @@
+#I DON'T  GET THE RISK FOR ANYTHING
+import urllib
+import os
+import appuifw
+
+SAVEDIR  = appuifw.query(u"Path to file:", "text")
+os.path.join(SAVEDIR)
+if not os.path.isdir(SAVEDIR):
+    os.makedirs(SAVEDIR)
+
+targetf = appuifw.query(u"Name the file:", "text")
+try:
+    f=open(targetf,'rt')
+    try:
+        content = f.read()
+        config=eval(content)
+        f.close()
+    except:
+        print 'Can not read file'
+except:
+    print 'Can not open file'
+
+urltarget = appuifw.query(u"The target url:", "text")
+target = urllib.urlopen(urltarget)
+data = target.read()
+file = open(targetf, 'wb')
+file.write(data, SAVEDIR)
+file.close
+print('The file was created:'), targetf
