1e1cef6c7720aa483b910e534242523f6b8e7231
BboyMUPO <sarkanyalex214@gmail.com>
2014-02-07 18:57:18 +0100

    First working version[updates comes later]

--- brute-force.py
+++ brute-force.py
@@ -1,15 +1,22 @@
 #I DON'T GET THE RISK FOR ANYTHING
 
-your_list = 'qwertzuiopasdfghjklíyxcvbnm' #latters you want to use in brute-force attack
-complete_list = []
-for current in xrange(2): #how long the password is default 2
-    a = [i for i in your_list]
-    for y in xrange(current):
-        a = [x+i for i in your_list for x in a]
-    complete_list = complete_list+a
+s = "a" #test password
+chars = [u"a", u"b", u"c", u"d", u"e", u"f", u"g", u"h", u"i", u"j", u"k", u"l", u"m", u"n", u"o", u"p", u"q", u"r", u"s", u"t", u"u", u"v", u"z", u"0", u"1", u"2", u"3", u"4", u"5", u"6", u"7", u"8", u"9"]
 
-testpass = ad
-pass = complete_list
+def brute(latters):
+    for i in latters:
+        asd = i
+        print(asd)
+        if asd == s:
+            print('PASSWORD FOUND:'),asd
+            return 0
+            
+print"=========="
+print"[BboyMUPO]"
+print"=========="
 
-if pass == testpass:
-    print('match:'), pass
+print"BRUTE FORCE!"
+
+print"Chacked passwords:"
+
+brute(chars)
